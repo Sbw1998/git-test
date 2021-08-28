f1ccd9f35ae06caeb5e925cdb84412915b1513cc
shibiwu <shi_bw@126.com>
2021-08-28 16:26:32 +0800

    second commit

--- src/main/java/com/itman/GitTest.java
+++ src/main/java/com/itman/GitTest.java
@@ -1,7 +1,13 @@
 package com.itman;
 
+
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
+
+
